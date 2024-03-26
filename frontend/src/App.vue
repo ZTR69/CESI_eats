cbbadb872d6150027a809655ed376cbb1192dfee
Arthur Colinet <arthur.colinet@viacesi.fr>
2024-03-26 14:29:25 +0100

    Init vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import logo from './assets/blacklogo.png';
+</script>
+
+<template>
+  <div class="container">
+    <img :src="logo" alt="Logo">
+    <h1>CESI eats</h1>
+  </div>
+</template>
+
+<style scoped>
+/* Vous pouvez ajouter ici vos styles personnalisés */
+</style>
