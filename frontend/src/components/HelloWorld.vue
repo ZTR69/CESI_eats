a16012ab6cbdb818e191132bcf824562de207905
Vassily DELMAS <vassily.delmas@cegedim.com>
2024-04-07 17:10:41 +0200

    début de squelette

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello World</h1>
+  <slot name="age"></slot>
+  <p>is</p>
+  <slot name="name"></slot>
+</template>
+
+
+<script setup>
+    const props = defineProps(['user']);
+</script>
