94b864e266322d380132c6b39fa0d444be37f8bd
Arthur Colinet <arthur.colinet@viacesi.fr>
2024-04-10 22:53:42 +0200

    fix Cors plicy

--- backend/user/server.js
+++ backend/user/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
+const cors = require('cors');
 
 // Definition du port
 const port = process.env.PORT || 5000
@@ -39,6 +40,8 @@ const app = express()
 // Accepter les données envoyées par formulaire
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// Utilisation du middleware CORS
+app.use(cors());
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
