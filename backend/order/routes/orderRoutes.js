cd25eefb7a8bd8671ce188c3006698482ff8e889
aLopes1 <77679111+aLopes1@users.noreply.github.com>
2024-04-10 11:13:28 +0200

    Update orderRoutes.js

--- backend/order/routes/orderRoutes.js
+++ backend/order/routes/orderRoutes.js
@@ -11,13 +11,13 @@ uppdateStatus,
 deleteItem,
 deleteOrder
 } = require('../controllers/orderController')
-router.get('/:orderID', getOrder)
 router.get('/all', getOrders)
+router.get('/:orderID', getOrder)
 router.get('/restaurant/:restaurantID', getRestaurantOrders)
 router.get('/restaurant/:restaurantID/pending', getRestaurantPendingOrders)
 router.post('/', addOrder)
 router.put('/:orderID/item', addItem)
 router.put('/:orderID/status', uppdateStatus)
-router.delete('/:orderID', deleteOrder)
 router.delete('/item/:orderID', deleteItem)
+router.delete('/:orderID', deleteOrder)
 module.exports = router
