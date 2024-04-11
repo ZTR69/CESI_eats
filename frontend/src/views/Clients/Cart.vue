bd53a4082dd5528985db7895da6fd4abe8b86330
Axelinhooo <axel.courtial@gmail.com>
2024-04-11 17:12:06 +0200

    Refactor Cart.vue component

--- frontend/src/views/Clients/Cart.vue
+++ frontend/src/views/Clients/Cart.vue
@@ -1,56 +1,48 @@
 <template>
-    <NavigationBar class="home" :is-connected="false" :showSearchBar="false" :show-cart="false" :showAddress="true"  :hide-button="true"/>
+    <NavigationBar class="home" :is-connected="false" :showSearchBar="false" :show-cart="false" :showAddress="true"
+        :hide-button="true" />
     <div class="cart">
-      <h1>Votre panier :</h1>
-      <MenuComponent
-        v-for="(item, index) in items"
-        :key="item.id"
-        :id="item.id"
-        :title="item.title"
-        :description="item.description"
-        :price="item.price"
-        :restaurantId="item.id"
-        :restaurantAddress="item.restaurantAddress"
-        :showAddToCartButton="false"
-        :showRemoveFromCartButton="true"
-        @removeFromCart="removeFromCart"
-      />
+        <h1>Votre panier :</h1>
+        <MenuComponent v-for="(item, index) in order.items" :key="index" :id="item.id" :title="item.title"
+            :description="item.description" :price="item.price" :showRemoveFromCartButton="true"
+            @removeFromCart="removeFromCart" />
         <div class="total-and-order-button">
-        <h1>Prix total : {{ total }} €</h1>
-        <button class="btn btn-primary" @click="showOrderForm = true">Valider la commande</button>
-            </div>
-            <div v-if="showOrderForm" class="order-form">
-                <h2>Informations de paiement</h2>
-                <form @submit.prevent="placeOrder">
-                    <label>
-                        Nom sur la carte :
-                        <input v-model="paymentInfo.cardName" required>
-                    </label>
-                    <label>
-                        Numéro de la carte :
-                        <input v-model="paymentInfo.cardNumber" required>
-                    </label>
-                    <label>
-                        Date d'expiration :
-                        <input v-model="paymentInfo.expiryDate" required>
-                    </label>
-                    <label>
-                        CVV :
-                        <input v-model="paymentInfo.cvv" required>
-                    </label>
-                    <button type="submit">Confirmer la commande</button>
-                    <p> {{this.items}} </p>
-                </form>
-            </div>
-            <div v-if="showModal" class="order-confirmation">
-                <p>Votre commande a été validée !</p>
-            </div>
+            <h1>Prix total : {{ total }} €</h1>
+            <button class="btn btn-primary" @click="showOrderForm = true">Valider la commande</button>
         </div>
-  </template>
+        <div v-if="showOrderForm" class="order-form">
+            <h2>Informations de paiement</h2>
+            <form @submit.prevent="placeOrder">
+                <label>
+                    Nom sur la carte :
+                    <input v-model="paymentInfo.cardName" required>
+                </label>
+                <label>
+                    Numéro de la carte :
+                    <input v-model="paymentInfo.cardNumber" required>
+                </label>
+                <label>
+                    Date d'expiration :
+                    <input v-model="paymentInfo.expiryDate" required>
+                </label>
+                <label>
+                    CVV :
+                    <input v-model="paymentInfo.cvv" required>
+                </label>
+                <button type="submit">Confirmer la commande</button>
+                <p> {{ this.order }} </p>
+            </form>
+        </div>
+        <div v-if="showModal" class="order-confirmation">
+            <p>Votre commande a été validée !</p>
+        </div>
+    </div>
+</template>
 
 <script>
 import MenuComponent from '@/components/MenuComponent.vue';
 import NavigationBar from '@/components/NavigationBar.vue';
+import apiService from '@/services/apiService';
 
 export default {
     components: {
@@ -59,42 +51,47 @@ export default {
     },
     data() {
         return {
-            items: [],
+            order: {items: []},
             showModal: false,
             showOrderForm: false,
-                paymentInfo: {
-                        cardName: '',
-                        cardNumber: '',
-                        expiryDate: '',
-                        cvv: ''
-                }
+            paymentInfo: {
+                cardName: '',
+                cardNumber: '',
+                expiryDate: '',
+                cvv: ''
+            }
+
+
         };
     },
     computed: {
         total() {
-            return this.items.reduce((acc, item) => acc + item.price, 0);
+            return this.order.items.reduce((acc, item) => acc + item.price, 0);
         }
     },
     methods: {
         removeFromCart(itemId) {
-            const index = this.items.findIndex(item => item.id === itemId);
+            const index = this.order.items.findIndex(item => item.id === itemId);
             if (index !== -1) {
-                this.items.splice(index, 1);
-                localStorage.setItem('cart', JSON.stringify(this.items));
+                this.order.items.splice(index, 1);
+                localStorage.setItem('cart', JSON.stringify(this.order.items));
             }
         },
         fetchCart() {
             const cart = JSON.parse(localStorage.getItem('cart')) || [];
-            this.items = cart;
+            this.order.items = cart;
         },
         placeOrder() {
-                console.log('Order placed', this.items);
-                console.log('Payment Info:', this.paymentInfo);
-                this.showModal = true;
-                this.showOrderForm = false;
-                // vider le panier
-                localStorage.removeItem('cart');
-                this.items = [];
+            apiService.fetchJsonWithToken('/api/orders', 'http://localhost:5010', 'POST', this.order)
+
+            console.log('Order placed', this.order);
+            console.log('Payment Info:', this.paymentInfo);
+            this.showModal = true;
+            this.showOrderForm = false;
+            // vider le panier
+            localStorage.removeItem('cart');
+            this.order.items = [];
+
         }
     },
     created() {
@@ -109,21 +106,26 @@ export default {
     padding: 20px;
     box-sizing: border-box;
 }
+
 .total-and-order-button {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
+
 .order-form {
     margin-top: 20px;
 }
+
 .order-form form {
     display: flex;
     flex-direction: column;
 }
+
 .order-form label {
     margin-bottom: 10px;
 }
+
 .order-form input {
     margin-top: 5px;
     margin-bottom: 15px;
